7ecee8f03eb854a4b272fe445f7377756091e06c
Arch Yi <843874092@qq.com>
2018-09-10 21:52:37 +0800

    fixed bug

--- src/main/java/isdc/isdcssm/support/WebConfig.java
+++ src/main/java/isdc/isdcssm/support/WebConfig.java
@@ -1,11 +1,15 @@
 package isdc.isdcssm.support;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 import java.util.List;
 
