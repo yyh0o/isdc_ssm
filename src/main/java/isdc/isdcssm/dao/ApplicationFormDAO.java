b5dafc2b0f1ffa280a87adfe9404118fa62f17d1
LightingX <609982707@qq.com>
2018-09-14 21:13:12 +0800

    the last bug.

--- src/main/java/isdc/isdcssm/dao/ApplicationFormDAO.java
+++ src/main/java/isdc/isdcssm/dao/ApplicationFormDAO.java
@@ -42,6 +42,28 @@ public interface ApplicationFormDAO extends tk.mybatis.mapper.common.Mapper<Appl
     @Select("select * from application_form where openid = #{openid}")
     ApplicationForm queryByOpenid(@Param("openid")String openid);
 
+    @Results({
+            @Result(column="id", property="id", jdbcType= JdbcType.INTEGER, id=true),
+            @Result(column="name", property="name", jdbcType=JdbcType.VARCHAR),
+            @Result(column="openid", property="openid", jdbcType=JdbcType.VARCHAR),
+            @Result(column="stu_id", property="stuId", jdbcType=JdbcType.BIGINT),
+            @Result(column="create_date", property="createDate", jdbcType=JdbcType.TIMESTAMP),
+            @Result(column="email", property="email", jdbcType=JdbcType.VARCHAR),
+            @Result(column="gender", property="gender", jdbcType=JdbcType.VARCHAR),
+            @Result(column="nationality", property="nationality", jdbcType=JdbcType.VARCHAR),
+            @Result(column="tel", property="tel", jdbcType=JdbcType.BIGINT),
+            @Result(column="interview", property="interview", jdbcType=JdbcType.TIMESTAMP),
+            @Result(column="block", property="block", jdbcType=JdbcType.BIT),
+            @Result(column="impression_score", property="impressionScore", jdbcType=JdbcType.DOUBLE),
+            @Result(column="integrated_score", property="integratedScore", jdbcType=JdbcType.DOUBLE),
+            @Result(column="interviewer", property="interviewer", jdbcType=JdbcType.VARCHAR),
+            @Result(column="pass", property="pass", jdbcType=JdbcType.BIT),
+            @Result(column="reason", property="reason", jdbcType=JdbcType.VARCHAR),
+            @Result(column="tech_score", property="techScore", jdbcType=JdbcType.DOUBLE),
+            @Result(column="description", property="description", jdbcType=JdbcType.LONGVARCHAR),
+            @Result(column="introduce", property="introduce", jdbcType=JdbcType.LONGVARCHAR),
+            @Result(column="judge", property="judge", jdbcType=JdbcType.LONGVARCHAR)
+    })
     @Select("select * from application_form where name is not null and create_date like '2018%'")
     List<ApplicationForm> findAll();
 }
